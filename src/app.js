d9edb36c9c0e46cb63ba62918e7e89bf70e836ae
hannah22409 <hawsomeness@yahoo.com>
2022-03-08 18:23:28 -0500

    website lookin slick buTTT js isn't working last 2 functionszsz

---
+++ src/app.js
@@ -0,0 +1,20 @@
+//this not done yet function for the AGENCY NAME on index html
+function notDone() {
+  alert("I'll change it when I have one");
+}
+let notDoneYet = document.querySelector(".notDone");
+notDoneYet.addEventListener("click", notDone);
+
+//this not done yet function for the SAMPLES on samples page
+function notDoneSample() {
+  alert("I'll add the samples when I have them");
+}
+let notDoneSamplee = document.querySelector(".notDoneSample");
+notDoneSamplee.addEventListener("click", notDoneSample);
+
+//this not done yet function for the LOGO on all 4 pages
+function notDoneLogo() {
+  alert("I'll change it to my logo when I make one");
+}
+let notDoneLogoo = document.querySelector(".notDoneLogo");
+notDoneLogoo.addEventListener("click", notDoneLogo);
